542265f7d2b148e09fcc654204df4091d7ed8590
Tyrone <tyroneti@cliostyle.com.br>
2022-04-02 22:12:36 -0300

    feat: core app updated with middleware and db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { PermissionMiddleware } from './middleware/permission.middleware';
+import { ClientModule } from './modules/client.module';
+import { LoginModule } from './modules/login.module';
+import { OperationModule } from './modules/operation.module';
 
+dotenv.config();
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB,
+      schema: process.env.DB_SCHEMA,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    ClientModule,
+    LoginModule,
+    OperationModule,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(PermissionMiddleware).forRoutes(
+      {
+        path: 'client',
+        method: RequestMethod.ALL,
+      },
+      {
+        path: 'operation',
+        method: RequestMethod.ALL,
+      },
+    );
+  }
+}
